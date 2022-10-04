908020763355fdf7753b5c223d725a890b3b966b
AhmedBilal <bilalshazad19@gmail.com>
2022-10-04 19:40:03 +0530

    1

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Project75' });
+  res.render('index', { title: 'Social Media' });
 });
 
 module.exports = router;
